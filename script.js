fdf82c65d851dbaf0eeb575f5319fd5049d778da
brino <bm13141455@gmail.com>
2022-08-20 16:37:32 -0300

    fix https

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let weather = {
   apiKey: "80e50b630da40967980ec41f1f304723",
   fetchWeather: function (city) {
     fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
         "&units=metric&appid=" +
         this.apiKey
